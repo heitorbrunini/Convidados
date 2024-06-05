dc21f424c9dc570e3a27edfb19f4d352fdb5a033
Heitor Brunini <68927093+heitorbrunini@users.noreply.github.com>
2024-06-05 16:47:06 -0300

    Guest register form

--- app/src/main/java/com/devbrunini/convidados/GuestFormActivity.kt
+++ app/src/main/java/com/devbrunini/convidados/GuestFormActivity.kt
@@ -1,20 +1,41 @@
 package com.devbrunini.convidados
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.devbrunini.convidados.databinding.ActivityGuestFormBinding
 
-class GuestFormActivity : AppCompatActivity() {
+class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityGuestFormBinding
+    private lateinit var  viewModel: GuestFormViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_guest_form)
+        binding = ActivityGuestFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel= ViewModelProvider(this)[GuestFormViewModel::class.java]
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.buttonSave.setOnClickListener(this)
+        binding.radioPresent.isChecked = true
+
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == binding.buttonSave.id){
+
         }
     }
 }
---
+++ app/src/main/java/com/devbrunini/convidados/GuestFormViewModel.kt
@@ -0,0 +1,6 @@
+package com.devbrunini.convidados
+
+import androidx.lifecycle.ViewModel
+
+class GuestFormViewModel: ViewModel() {
+}
