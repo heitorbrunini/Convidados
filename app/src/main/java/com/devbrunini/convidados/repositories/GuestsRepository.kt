8d013b22ac8034336665344473d06815fb20c0e6
Heitor Brunini <68927093+heitorbrunini@users.noreply.github.com>
2024-06-10 19:25:35 -0300

    Repository update, delete, getAll

--- app/src/main/java/com/devbrunini/convidados/repositories/GuestsRepository.kt
+++ app/src/main/java/com/devbrunini/convidados/repositories/GuestsRepository.kt
@@ -2,6 +2,7 @@ package com.devbrunini.convidados.repositories
 
 import android.content.ContentValues
 import android.content.Context
+import com.devbrunini.convidados.constants.DBConstants
 import com.devbrunini.convidados.model.GuestsModel
 
 class GuestsRepository private constructor(context: Context){
@@ -29,14 +30,61 @@ class GuestsRepository private constructor(context: Context){
             values.put("name", guest.name)
             values.put("presence", if (guest.presence) 1 else 0)
 
-            db.insert("Guests", null, values)
+            db.insert(DBConstants.GUESTS.TABLE_NAME, null, values)
             return true
         } catch (e: Exception) {
             return false
         }
     }
 
-    fun update(){
+    fun update(guest: GuestsModel): Boolean {
+
+        try {
+            val db = guestsDataBase.writableDatabase
+            val values = ContentValues()
+            val args = arrayOf(guest.id.toString())
+            values.put("name", guest.name)
+            values.put("presence", if (guest.presence) 1 else 0)
+            db.update(DBConstants.GUESTS.TABLE_NAME, values, "id = ?", args)
+            return true
+        }catch (e: Exception){
+            return false
+        }
+
+    }
+
+    fun delete(id: Int): Boolean {
+        try {
+            val db = guestsDataBase.writableDatabase
+            val args = arrayOf(id.toString())
+            db.delete(DBConstants.GUESTS.TABLE_NAME, "id = ?", args)
+            return true
+        }catch (e: Exception){
+            return false
+        }
+    }
+
+    fun getAll(): List<GuestsModel> {
+        val db = guestsDataBase.readableDatabase
+        val args = arrayOf(
+            DBConstants.COLUMNS.ID,
+            DBConstants.COLUMNS.NAME,
+            DBConstants.COLUMNS.PRESENCE
+        )
+
+        val list = ArrayList<GuestsModel>()
+        val cursor = db.rawQuery("SELECT * FROM " + DBConstants.GUESTS.TABLE_NAME, args)
+
+        if (cursor.count > 0 && cursor!=null) {
+            while (cursor.moveToNext()) {
+                val id = cursor.getInt(cursor.getColumnIndex(DBConstants.COLUMNS.ID))
+                val name = cursor.getString(cursor.getColumnIndex(DBConstants.COLUMNS.NAME))
+                val presence = cursor.getInt(cursor.getColumnIndex(DBConstants.COLUMNS.PRESENCE))
+                list.add(GuestsModel(id, name, presence == 1))
+            }
+            cursor.close()
+        }
+        return list
 
     }
 
